167dfaf8c006b99ef284fbda855789a42dcb9840
BeyondJS08 <jsvcbusiness08@gmail.com>
2026-06-08 19:17:41 -0700

    feat: import all models for Alembic

---
+++ backend-airmed/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.user import User
+from app.models.appointment import Appointment
+from app.models.availability import Availability
+from app.models.service import Service
+from app.models.refresh_token import RefreshToken
